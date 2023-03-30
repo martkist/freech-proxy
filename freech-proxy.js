c18f3e0353675d3677d35b29b24f7224de39da49
blockmartkist <blockmartkist@protonmail.com>
2023-03-30 19:32:52 +0200

    Proxy certain required headers

--- freech-proxy.js
+++ freech-proxy.js
@@ -7,8 +7,7 @@ var console = require("console");
 var app = express();
 var cors = require('cors');
 
-function handlePortAccessError()
-{
+function handlePortAccessError() {
     console.log("Error: Node.js doesn't allow access to ports lower than 1024 in normal user mode.");
     console.log("   Possible solutions:");
     console.log("       1 - Edit settings.json and change \"http_port\": 80 to a higher number. It will then run under any user.");
@@ -16,37 +15,30 @@ function handlePortAccessError()
     process.exit(1);
 }
 
-try
-{
+try {
     var settings = JSON.parse(fs.readFileSync("settings.json"));
 }
-catch(e)
-{
+catch (e) {
     console.log("Error: Configuration file settings.json couldn't be parsed.\nSee Troubleshooting section in README.md for instructions.");
     process.exit(1);
 }
 
-if(settings.Server.enable_https)
-{
-    try
-    {
-        var privateKey  = fs.readFileSync(settings.Server.ssl_key_file).toString();
+if (settings.Server.enable_https) {
+    try {
+        var privateKey = fs.readFileSync(settings.Server.ssl_key_file).toString();
     }
-    catch(e)
-    {
+    catch (e) {
         console.log("Error: unable to load SSL key. Please edit setting.json and put the correct path to your SSL private key file into \"ssl_key_file.\"");
         process.exit(1);
     }
-    try
-    {
+    try {
         var certificate = fs.readFileSync(settings.Server.ssl_certificate_file).toString();
     }
-    catch(e)
-    {
+    catch (e) {
         console.log("Error: unable to load SSL certificate. Please edit setting.json and put the correct path to your SSL certificate file into \"ssl_certificate_file.\"");
         process.exit(1);
     }
-    var credentials = {key: privateKey, cert: certificate};
+    var credentials = { key: privateKey, cert: certificate };
 }
 
 maxCallsPerMinute = {};
@@ -61,23 +53,21 @@ var connectionErrorMessageDisplayed = false;
 
 var auth = "Basic " + new Buffer(settings.RPC.user + ":" + settings.RPC.password).toString("base64");
 
-settings.CallLimits.forEach(function(x) {
+settings.CallLimits.forEach(function (x) {
     maxCallsPerMinute[x.name] = x.maxPerMinute;
     maxCallsPerMinutePerIP[x.name] = x.maxPerMinutePerIP;
     callsRemaining[x.name] = x.maxPerMinute;
     droppedCallsCounter[x.name] = 0;
 });
 
-CounterInstance = function()
-{
-    this.overLimit=0;
-    this.invalidRequests=0;
-    this.forbiddenCalls=0;
-    this.callsRemaining={};
-    for (x in maxCallsPerMinutePerIP)
-    {
-        if(maxCallsPerMinutePerIP[x]!==null)
-        this.callsRemaining[x]=maxCallsPerMinutePerIP[x];
+CounterInstance = function () {
+    this.overLimit = 0;
+    this.invalidRequests = 0;
+    this.forbiddenCalls = 0;
+    this.callsRemaining = {};
+    for (x in maxCallsPerMinutePerIP) {
+        if (maxCallsPerMinutePerIP[x] !== null)
+            this.callsRemaining[x] = maxCallsPerMinutePerIP[x];
     }
 }
 
@@ -85,176 +75,160 @@ require("log-timestamp");
 
 app.use(cors());
 
-app.get("*", function(request, response)
-{
-    if(settings.Server.enable_https&&request.protocol=="http")
-    {
-        if(settings.Server.https_port==443)
-            secureUrl="https://"+request.host+request.path;
+app.get("*", function (request, response) {
+    if (settings.Server.enable_https && request.protocol == "http") {
+        if (settings.Server.https_port == 443)
+            secureUrl = "https://" + request.host + request.path;
         else
-            secureUrl="https://"+request.host+":"+settings.Server.https_port+request.path;
-        response.writeHead(302, {"Location": secureUrl});
+            secureUrl = "https://" + request.host + ":" + settings.Server.https_port + request.path;
+        response.writeHead(302, { "Location": secureUrl });
         response.end();
         return;
     }
 
     request.headers.Authorization = auth;
-    
-    var webProxy = http.request({host: settings.RPC.host, port: settings.RPC.port, method: request.method, path: request.path, headers: request.headers}, function (proxy_res)
-    {
-        proxy_res.pipe(response, {end: true});
+
+    const options = { host: settings.RPC.host, port: settings.RPC.port, method: request.method, path: request.path, headers: request.headers };
+    var webProxy = http.request(options, function (proxy_res) {
+        // Copy specific headers from the proxy response to the actual response
+        if (proxy_res.headers['content-length']) {
+            response.setHeader('content-length', proxy_res.headers['content-length']);
+        }
+
+        if (proxy_res.headers['content-security-policy']) {
+            response.setHeader('content-security-policy', proxy_res.headers['content-security-policy']);
+        }
+
+        if (proxy_res.headers['content-type']) {
+            response.setHeader('content-type', proxy_res.headers['content-type']);
+        }
+
+        // Set the status code of the actual response to match the proxy response
+        response.status(proxy_res.statusCode);
+        proxy_res.pipe(response, { end: true });
     });
-    
-    webProxy.on("error", function(error)
-    {
-        if(!connectionErrorMessageDisplayed)
-        {
+
+    webProxy.on("error", function (error) {
+        if (!connectionErrorMessageDisplayed) {
             console.log("Error: cannot connect to freechd.\nSee Troubleshooting section in README.md for instructions.");
-            connectionErrorMessageDisplayed=true;
+            connectionErrorMessageDisplayed = true;
         }
         response.send(502);
     });
-    
-    request.pipe(webProxy, {end: true});
+
+    request.pipe(webProxy, { end: true });
 });
 
-app.post("/", function(request, response)
-{       
+app.post("/", function (request, response) {
     request.rawBody = "";
     request.setEncoding("utf8");
     request.headers.Authorization = auth;
 
-    request.addListener("data", function(chunk)
-    {
+    request.addListener("data", function (chunk) {
         request.rawBody += chunk;
     });
 
-    request.addListener("end", function()
-    {
+    request.addListener("end", function () {
         var remoteIP = request.connection.remoteAddress;
-        
-        if(perIPCounter[remoteIP]===undefined)
-        {    
-            perIPCounter[remoteIP]=new CounterInstance();
+
+        if (perIPCounter[remoteIP] === undefined) {
+            perIPCounter[remoteIP] = new CounterInstance();
         }
-        try
-        {
-            bodyJson=JSON.parse(request.rawBody);
-            rpcMethod=bodyJson.method;
+        try {
+            bodyJson = JSON.parse(request.rawBody);
+            rpcMethod = bodyJson.method;
         }
-        catch(e)
-        {
+        catch (e) {
             perIPCounter[remoteIP].invalidRequests++;
             invalidRequestCounter++;
             return;
         }
-        if(maxCallsPerMinute[rpcMethod]===undefined || maxCallsPerMinute[rpcMethod]===0)
-        {
+        if (maxCallsPerMinute[rpcMethod] === undefined || maxCallsPerMinute[rpcMethod] === 0) {
             perIPCounter[remoteIP].forbiddenCalls++;
             forbiddenCallCounter++;
             return;
         }
-        if(maxCallsPerMinute[rpcMethod]!==null)
-        {
-            if(callsRemaining[rpcMethod]<1)
-            {
+        if (maxCallsPerMinute[rpcMethod] !== null) {
+            if (callsRemaining[rpcMethod] < 1) {
                 droppedCallsCounter[rpcMethod]++;
                 return;
             }
-            else
-            {
+            else {
                 callsRemaining[rpcMethod]--;
             }
         }
-    
-        if(maxCallsPerMinutePerIP[rpcMethod]!==null)
-        {    
-            if(perIPCounter[remoteIP].callsRemaining[rpcMethod]<1)
-            {
+
+        if (maxCallsPerMinutePerIP[rpcMethod] !== null) {
+            if (perIPCounter[remoteIP].callsRemaining[rpcMethod] < 1) {
                 perIPCounter[remoteIP].overLimit++;
                 return;
             }
-            else
-            {
+            else {
                 perIPCounter[remoteIP].callsRemaining[rpcMethod]--;
             }
         }
-    
-        var rpcProxy = http.request({host: settings.RPC.host, port: settings.RPC.port, method: request.method, headers: request.headers}, function(proxy_res)
-        {
-            proxy_res.on("data", function(chunk)
-            {
+
+        var rpcProxy = http.request({ host: settings.RPC.host, port: settings.RPC.port, method: request.method, headers: request.headers }, function (proxy_res) {
+            proxy_res.on("data", function (chunk) {
                 response.write(chunk, "binary");
             });
 
-            proxy_res.on("end", function(chunk)
-            {
+            proxy_res.on("end", function (chunk) {
                 response.end();
             });
-        
-            proxy_res.on("error", function(error)
-            {
-                if(!connectionErrorMessageDisplayed)
-                {
+
+            proxy_res.on("error", function (error) {
+                if (!connectionErrorMessageDisplayed) {
                     console.log("Error: cannot connect to freechd.\nSee Troubleshooting section in README.md for instructions.");
-                    connectionErrorMessageDisplayed=true;
+                    connectionErrorMessageDisplayed = true;
                 }
                 response.send(502);
             });
 
             response.writeHead(proxy_res.statusCode, proxy_res.headers);
         });
-    
+
         rpcProxy.write(request.rawBody, "binary");
         rpcProxy.end();
     });
 });
 
-if(settings.Server.enable_https)
+if (settings.Server.enable_https)
     https.createServer(credentials, app).listen(settings.Server.https_port).on("error", handlePortAccessError);
 
 http.createServer(app).listen(settings.Server.http_port).on("error", handlePortAccessError);
 
 
-setInterval(function()
-{
-    for(method in maxCallsPerMinute)
-    {
+setInterval(function () {
+    for (method in maxCallsPerMinute) {
         callsRemaining[method] = maxCallsPerMinute[method];
-        if(droppedCallsCounter[method]!==0)
-        {
-            console.log("Dropped "+droppedCallsCounter[method]+" calls to "+method+" over the limit of "+maxCallsPerMinute[method]);
+        if (droppedCallsCounter[method] !== 0) {
+            console.log("Dropped " + droppedCallsCounter[method] + " calls to " + method + " over the limit of " + maxCallsPerMinute[method]);
             droppedCallsCounter[method] = 0;
         }
     };
-    if(invalidRequestCounter!==0)
-    {
-        console.log("Received "+invalidRequestCounter+" invalid POST requests.");
+    if (invalidRequestCounter !== 0) {
+        console.log("Received " + invalidRequestCounter + " invalid POST requests.");
         invalidRequestCounter = 0;
     }
-    if(forbiddenCallCounter!==0)
-    {
-        console.log("Denied "+forbiddenCallCounter+" attempts to access forbidden API calls.");
+    if (forbiddenCallCounter !== 0) {
+        console.log("Denied " + forbiddenCallCounter + " attempts to access forbidden API calls.");
         forbiddenCallCounter = 0;
     }
-    
-    for(ip in perIPCounter)
-    {
-        if(perIPCounter[ip].overLimit>=settings.LogAsAttackThreshold.callsOverLimits)
-        {
-            console.log("IP "+ip+" tried to send "+perIPCounter[ip].overLimit+" calls more than the limits allow.");
+
+    for (ip in perIPCounter) {
+        if (perIPCounter[ip].overLimit >= settings.LogAsAttackThreshold.callsOverLimits) {
+            console.log("IP " + ip + " tried to send " + perIPCounter[ip].overLimit + " calls more than the limits allow.");
         };
-        if(perIPCounter[ip].invalidRequests>=settings.LogAsAttackThreshold.invalidRequests)
-        {
-            console.log("IP "+ip+" sent "+perIPCounter[ip].invalidRequests+" invalid request that couldn't be parsed.");
+        if (perIPCounter[ip].invalidRequests >= settings.LogAsAttackThreshold.invalidRequests) {
+            console.log("IP " + ip + " sent " + perIPCounter[ip].invalidRequests + " invalid request that couldn't be parsed.");
         };
-        if(perIPCounter[ip].forbiddenCalls>=settings.LogAsAttackThreshold.forbiddenCalls)
-        {
-            console.log("IP "+ip+" tried to send "+perIPCounter[ip].forbiddenCalls+" calls to forbidden functions.");
+        if (perIPCounter[ip].forbiddenCalls >= settings.LogAsAttackThreshold.forbiddenCalls) {
+            console.log("IP " + ip + " tried to send " + perIPCounter[ip].forbiddenCalls + " calls to forbidden functions.");
         };
     }
-    
+
     perIPCounter = {};
     connectionErrorMessageDisplayed = false;
-    
+
 }, 60000);
